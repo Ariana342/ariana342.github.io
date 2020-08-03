bfdd275b0a6c7a849711c712f53f68fdbaf06dba
Ariana Campbell <ariana.campbell@macaulay.cuny.edu>
2020-08-03 18:17:42 -0400

    Add files via upload

--- CSCI_355_Term_Project/action.php
+++ CSCI_355_Term_Project/action.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html>
 	<body>
-		<?php>
-		// Check if the form is submitted
-		if ( isset( $_POST['submit'] ) ) {
-			$userName = $_POST['username'];
-			echo "User Name: " . $userName . "<br />";
-			$baseName = $_POST['basename'];
-			echo "Base Name: " . $baseName . "<br />";
-			$base = $_POST['base'];
-			echo "Base : " . $base . "<br />";
-		}
+		<?php
+			if ( isset( $_POST['submit'] ) ) {
+				$userName = $_POST['username'];
+				echo "User Name: " . $userName . "<br />";
+				$baseName = $_POST['basename'];
+				echo "Base Name: " . $baseName . "<br />";
+				$base = $_POST['base'];
+				echo "Base : " . $base . "<br />";
+			}
 		?>
 	</body>
 </html>
