fa0a8137e3377924544cf93bdd715ce4d65b5590
Ariana Campbell <ariana.campbell@macaulay.cuny.edu>
2020-08-03 18:15:34 -0400

    Add files via upload

--- action.php
+++ action.php
@@ -1,47 +1,15 @@
 <!DOCTYPE html>
 <html>
 	<body>
-		<?php>
-		// Check if the form is submitted
-		if ( isset( $_POST['submit'] ) ) {
-
-		// retrieve the form data by using the element's name attributes value as key
-
-		echo '<h2>form data retrieved by using the $_REQUEST variable<h2/>'
-
-		$username = $_REQUEST['username'];
-		$basename = $_REQUEST['basename'];
-
-		// display the results
-		echo 'Your name is ' . $basename .' ' . $username;
-
-		// check if the post method is used to submit the form
-
-		if ( filter_has_var( INPUT_POST, 'submit' ) ) {
-
-		echo '<h2>form data retrieved by using $_POST variable<h2/>'
-
-		$username = $_POST['username'];
-		$basename = $_POST['basename'];
-
-		// display the results
-		echo 'Your name is ' . $basename .' ' . $username;
-		}
-
-		// check if the get method is used to submit the form
-
-		if ( filter_has_var( INPUT_GET, 'submit' ) ) {
-
-		echo '<h2>form data retrieved by using $_GET variable<h2/>'
-
-		$username = $_GET['username'];
-		$basename = $_GET['basename'];
-		}
-
-		// display the results
-		echo 'Your name is ' . $basename .' ' . $username;
-		exit;
-		}
+		<?php
+			if ( isset( $_POST['submit'] ) ) {
+				$userName = $_POST['username'];
+				echo "User Name: " . $userName . "<br />";
+				$baseName = $_POST['basename'];
+				echo "Base Name: " . $baseName . "<br />";
+				$base = $_POST['base'];
+				echo "Base : " . $base . "<br />";
+			}
 		?>
 	</body>
 </html>
